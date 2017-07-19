67db82371380179e853cf65cad0b901c16658e6e
Carmine Belvedere <cb00327@astro1.ph.surrey.ac.uk>
2017-07-19 17:04:20 +0100

    add GC view

--- catalogue/views.py
+++ catalogue/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from catalogue.models import Observation, Reference
+from catalogue.models import GlobularCluster, Observation, Reference
 import django_tables2 as tables
+from django.shortcuts import get_object_or_404, render
 
 class ObservationTable(tables.Table):
     class Meta:
@@ -14,4 +15,10 @@ def index(request):
 #    table = ObservationTable(Observation.objects.all())
 #    table.paginate(paginate=False)
     return render(request, 'index.html', {'observations': Observation.objects.all(), 'references': Reference.objects.all()})
+
+
+def detail(request, cluster_id_id):
+    cluster = get_object_or_404(GlobularCluster, pk=cluster_id_id)
+    return render(request, 'detail.html', {'observations': Observation.objects.filter(cluster_id=cluster)})
+
 
