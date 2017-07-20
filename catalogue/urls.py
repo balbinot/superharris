28ac07cd388f21951229eaf90d5da71a0c79cd2c
Carmine Belvedere <cb00327@astro1.ph.surrey.ac.uk>
2017-07-20 16:50:09 +0100

    add ref page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-     url(r'^GCID(?P<cluster_id_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^GCID(?P<cluster_id_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^references/$', views.references, name='references'),
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -17,8 +17,16 @@ def index(request):
     return render(request, 'index.html', {'observations': Observation.objects.all(), 'references': Reference.objects.all()})
 
 
+def references(request):
+#    table = ObservationTable(Observation.objects.all())
+#    table.paginate(paginate=False)
+    return render(request, 'references.html', {'references': Reference.objects.all()})
+
+
 def detail(request, cluster_id_id):
     cluster = get_object_or_404(GlobularCluster, pk=cluster_id_id)
     return render(request, 'detail.html', {'observations': Observation.objects.filter(cluster_id=cluster)})
+
+
 
 
